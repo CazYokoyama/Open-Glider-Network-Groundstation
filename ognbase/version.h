7cb0f7d9b004b3363bfca37056a42db9b8ef9da9
roema <manuel.roesel@ros-it.ch>
2020-12-28 23:29:49 +0100

    corr. version.h

--- ognbase/version.h
+++ ognbase/version.h
@@ -1,11 +1,11 @@
 //The version number conforms to semver.org format
 #define _VERSION_MAJOR 0
-#define _VERSION_MINOR 0
-#define _VERSION_PATCH 12
-#define _VERSION_BUILD 12
+#define _VERSION_MINOR 1
+#define _VERSION_PATCH 0
+#define _VERSION_BUILD 1
 #define _VERSION_DATE "28/12/2020"
 #define _VERSION_TIME "23:06:20"
-#define _VERSION_ONLY "0.0.12"
-#define _VERSION_NOBUILD "0.0.12 (28/12/2020)"
-#define _VERSION "0.0.12-12"
+#define _VERSION_ONLY "0.1.0"
+#define _VERSION_NOBUILD "0.1.0 (28/12/2020)"
+#define _VERSION "0.1.0-1"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
