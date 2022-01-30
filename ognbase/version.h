dab61f88c10e502ebe33d3e8cd8d5a6fcc1d897a
Manuel Roesel <manuel.roesel@ros-it.ch>
2022-01-30 09:54:05 +0100

    correcting version number

--- ognbase/version.h
+++ ognbase/version.h
@@ -2,10 +2,10 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 1
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 26
-#define _VERSION_DATE "31/10/2021"
-#define _VERSION_TIME "12:11:36"
+#define _VERSION_BUILD 27
+#define _VERSION_DATE "30/01/2022"
+#define _VERSION_TIME "09:39:22"
 #define _VERSION_ONLY "0.1.0"
-#define _VERSION_NOBUILD "0.1.0 (31/10/2021)"
-#define _VERSION "0.1.0-26"
+#define _VERSION_NOBUILD "0.1.0 (30/01/2022)"
+#define _VERSION "0.1.0-27"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
